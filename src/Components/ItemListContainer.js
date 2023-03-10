a76628e72434b8bd6208037a1bfdf6002e25223a
BLANCO, PABLO NICOLAS <pblanco@nacionservicios.com.ar>
2023-03-10 11:27:00 -0300

    agrego colores

--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -1,8 +1,15 @@
+const styles = {
+    fontFamily: 'Courier',
+    color: 'blue',
+    backgroundColor: 'red',
+    opacity: 0.3,
+}
+
 function ItemListContainer (props){
 return (
 
     <div>
-    <p>{props.mensaje}</p>
+    <h1 style={styles}>{props.mensaje}</h1>
     </div>
 );
 
