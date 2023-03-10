cb9d01f07bae067f0bc28ba76dd9affa3020f095
BLANCO, PABLO NICOLAS <pblanco@nacionservicios.com.ar>
2023-03-10 11:34:22 -0300

    agrego format al codigo

--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -1,19 +1,16 @@
 const styles = {
-    fontFamily: 'Courier',
-    color: 'blue',
-    backgroundColor: 'red',
-    opacity: 0.3,
-}
-
-function ItemListContainer (props){
-return (
-
-    <div>
-    <h1 style={styles}>{props.mensaje}</h1>
-    </div>
-);
-
-
+  fontFamily: "sans-serif",
+  color: "black",
+  backgroundColor: "red",
+  opacity: 0.6,
 };
+
+function ItemListContainer(props) {
+  return (
+    <div>
+      <h1 style={styles}>{props.mensaje}</h1>
+    </div>
+  );
+}
 
 export default ItemListContainer;
